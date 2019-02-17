30e37897a533c43c85236189b48c3a64b846a1b2
Ajay Sharma <ajayks@msn.com>
2019-02-17 17:30:46 -0500

    Add a way for the graph to pull data from a function

--- calc-app/src/components/Graph.js
+++ calc-app/src/components/Graph.js
@@ -1,28 +1,22 @@
 import {LineChart} from 'react-easy-chart';
 import React from 'react'
 
-function getData(min, max, step, x, length, width) {
+function getData(min, max, step, length, width) {
+  var dataArray = []
 
-  var test = LineChart.data = [
-    [
-      { x: 1, y: 20 },
-      { x: 2, y: 10 },
-      { x: 3, y: 25 }
-    ], [
-      { x: 1, y: 10 },
-      { x: 2, y: 12 },
-      { x: 3, y: 4 }
-    ]
-  ];
-  console.log(test[0,0]);
-  return test;
+  for(var i = min; i < max; i += step) {
+    var point = LineChart.data = { x: i, y:(length - 2*i) *(width - 2 *i) *i };
+    dataArray.push(point);
+  }
+  console.log(dataArray);
+  return [dataArray];
 }
 
 
 export default function Graph() {
   return (
     <div>
-      <LineChart data={getData(0, 10, 1, 2, 30, 60)}/>
+      <LineChart data={getData(0, 30, 0.1, 30, 60)}/>
     </div>
   )
 }
