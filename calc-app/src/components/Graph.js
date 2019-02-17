4b977985e9918c0964644264a25548f35d17183f
Ajay Sharma <ajayks@msn.com>
2019-02-17 16:41:17 -0500

    Update Graph.js

--- calc-app/src/components/Graph.js
+++ calc-app/src/components/Graph.js
@@ -1,29 +1,26 @@
 import {LineChart} from 'react-easy-chart';
 import React from 'react'
 
-/*
-getData: (min, max, step, x, length, width) => {
-  var dataArray = []
+function getData(min, max, step, x, length, width) {
+  
+  return LineChart.data = [
+    [
+      { x: 1, y: 20 },
+      { x: 2, y: 10 },
+      { x: 3, y: 25 }
+    ], [
+      { x: 1, y: 10 },
+      { x: 2, y: 12 },
+      { x: 3, y: 4 }
+    ]
+  ];
 }
-*/
 
-export default function Header() {
+
+export default function Graph() {
   return (
     <div>
-        <LineChart data={[
-      [
-        { x: 1, y: 20 },
-        { x: 2, y: 10 },
-        { x: 3, y: 25 }
-      ]
-    ]}/>
+        <LineChart data={getData(0, 10, 1, 2, 30, 60)}/>
     </div>
   )
 }
-
-const headerStyle = {
-    background: '#333',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px'
-  }
