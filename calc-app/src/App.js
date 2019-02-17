2ec80e290b014aba7d288ee83b6b07198b3370aa
Ajay Sharma <ajayks@msn.com>
2019-02-17 10:46:00 -0500

    Added Footer
    Someone Figure out what to add inside the footer

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
           <p>
             BODY
           </p>
+          <Footer />
       </div>
     );
   }
---
+++ calc-app/src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer style ={footerStyle}>
+      <footer>Created By: Ajay Sharma, Anupryium (Last Name), Neel Shah, Aatman Shah</footer>
+    </footer>
+  )
+}
+
+const footerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+  }
