9f5bdd90c17f39dc1cff672d1c1a5ed868fd2bf9
Ajay Sharma <ajayks@msn.com>
2019-02-17 10:28:55 -0500

    Added Header and Removed Unessesary Files

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './MathForYou.svg';
 import './App.css';
+import Header from './components/layout/Header';
+
 
 class App extends Component {
   render() {
+    console.log("Hello")
     return (
+<<<<<<< HEAD
+      <div>
+          <Header />
+=======
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
+<<<<<<< HEAD
+>>>>>>> be6a48944bd69b6edc2019aba6687d59c7f03480
+=======
+>>>>>>> be6a48944bd69b6edc2019aba6687d59c7f03480
           <p>
-            <br/>FUCK ME AATMAN!!!!!!
-            <br/>PLEASE POPPY
+            <br/>Christian Channel
+            <br/>
           </p>
+<<<<<<< HEAD
+=======
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,7 +32,9 @@ class App extends Component {
           >
             Learn React
           </a>
+
         </header>
+>>>>>>> be6a48944bd69b6edc2019aba6687d59c7f03480
       </div>
     );
   }
