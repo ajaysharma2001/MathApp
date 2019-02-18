2a20f86f32fe88122346a1cc17dffcd10d4053f6
Neel Shah <neelsrocks14@gmail.com>
2019-02-17 20:47:24 -0500

    nothing

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -28,6 +28,9 @@ class App extends Component {
             <div style = {col}>
               {/* Where the 3D display will go */}
             </div>
+            <div style = {col}>
+              <Variables />
+            </div>
           </div>
           
             
