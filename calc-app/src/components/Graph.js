20ae6306f941e98731633b62f893dace4f56f6d3
Ajay Sharma <ajayks@msn.com>
2019-02-18 20:00:57 -0500

    graph updates

--- calc-app/src/components/Graph.js
+++ calc-app/src/components/Graph.js
@@ -1,20 +1,9 @@
 import {LineChart} from 'react-easy-chart';
 import React, { Component } from 'react';
 
-var maxVolume = 0;
 
-function getData(min, max, step, length, width) {
-  var dataArray = []
+var graphData
 
-  for(var i = min; i < max; i += step) {
-    var point = LineChart.data = { x: i, y:(length - 2*i) *(width - 2 *i) *i };
-    if(point.y > maxVolume) {
-      maxVolume = point.y
-    }
-    dataArray.push(point);
-  }
-  return [dataArray];
-}
 
 function getCurrentPoint(length, width, x) {
   var point = LineChart.data = { x: x, y:(length - 2*x) *(width - 2 *x) *x };
@@ -22,8 +11,23 @@ function getCurrentPoint(length, width, x) {
   return [arr];
 }
 
+var currentMaxVolume = 5195.987999999999;
 
 class Graph extends Component {
+
+  getData = (min, max, step, length, width) => {
+    var dataArray = []
+    currentMaxVolume = 0;
+    for(var i = min; i < max; i += step) {
+      var point = LineChart.data = { x: i, y:(length - 2*i) *(width - 2 *i) *i };
+      if(point.y > currentMaxVolume) {
+        currentMaxVolume = point.y
+      }
+      dataArray.push(point);
+    }
+    graphData = [dataArray];
+    return [dataArray];
+  }
 
   maxCutout(length, width) {
     if(length > width) {
@@ -38,10 +42,10 @@ class Graph extends Component {
     return (
       <div>
         <div style = {overlap}>
-          <LineChart margin={{top: 10, right: 10, bottom: 50, left: 100}} style={{ '.label': { fill: 'black' } }} data={getData(0, 15, 0.1, this.props.length, this.props.width)} axes grid verticalGrid axisLabels={{x: 'Cutout Size', y: 'Volume'}} width={400} height={400} interpolate={'cardinal'}/>
+          <LineChart margin={{top: 10, right: 10, bottom: 50, left: 100}} style={{ '.label': { fill: 'black' } }} data={this.getData(0, this.maxCutout(this.props.length, this.props.width), 0.1, this.props.length, this.props.width)} axes grid verticalGrid axisLabels={{x: 'Cutout Size', y: 'Volume'}} width={400} height={400} interpolate={'cardinal'}/>
         </div>
         <div style = {overlap}>
-          <LineChart margin={{top: 10, right: 10, bottom: 50, left: 100}} style={{ '.label': { fill: 'black' } }} data={getCurrentPoint(this.props.length,this.props.width,this.props.x)} width={400} height={400} dataPoints xDomainRange={[0, this.maxCutout(this.props.length, this.props.width)]} yDomainRange={[0, maxVolume]}/>
+          <LineChart margin={{top: 10, right: 10, bottom: 50, left: 100}} axes style={{ '.label': { fill: 'black' } }} data={getCurrentPoint(this.props.length,this.props.width,this.props.x)} width={400} height={400} dataPoints xDomainRange={[0, this.maxCutout(this.props.length, this.props.width)]} yDomainRange={[0, currentMaxVolume]}/>
         </div>
       </div>
     )
