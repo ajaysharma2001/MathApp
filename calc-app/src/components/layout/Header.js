63735af99b3f1e47e7be45a2b1760fe73bafacc8
Ajay Sharma <ajayks@msn.com>
2019-02-18 20:32:58 -0500

    Update Header.js

--- calc-app/src/components/layout/Header.js
+++ calc-app/src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header style ={headerStyle}>
-      <h1>Optimization App</h1>
+      <h1>Optimization Application</h1>
     </header>
   )
 }
